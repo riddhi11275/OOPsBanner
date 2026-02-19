cd50f1835c7a88407db902a4fbd2d393e6b0a6e6
Riddhi Sharma <riddhisharma89900@gmail.com>
2026-02-19 10:23:34 +0530

    UC3 - String.join implemented

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,35 @@
-/**
- * OOPSBannerApp UC2 - OOPS Banner Display Application
- * This class displays the OOPS acronym as a banner in console.
- *
- * Author: Riddhi
- * Version: 2.0
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" OOOO   OOOO  PPPP   SSSS ");
-        System.out.println("O    O O    O P   P S     ");
-        System.out.println("O    O O    O PPPP   SSS  ");
-        System.out.println("O    O O    O P         S ");
-        System.out.println(" OOOO   OOOO  P     SSSS  ");
+        // Line 1
+        System.out.println(
+            String.join(" ",
+                " ***** ", " ***** ", " ***** ", " ***** ")
+        );
 
+        // Line 2
+        System.out.println(
+            String.join(" ",
+                "*     *", "*     *", "*     *", "*     *")
+        );
+
+        // Line 3
+        System.out.println(
+            String.join(" ",
+                "*     *", "*     *", "*     *", "*     *")
+        );
+
+        // Line 4
+        System.out.println(
+            String.join(" ",
+                "*     *", "*     *", "*     *", "*     *")
+        );
+
+        // Line 5
+        System.out.println(
+            String.join(" ",
+                " ***** ", " ***** ", " ***** ", " ***** ")
+        );
     }
 }
